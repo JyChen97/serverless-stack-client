d8dfaf93806157ddd8b55fb310763c68c49d57bd
JyChen97 <Jiayingchen1997@gmail.com>
2020-03-14 10:08:51 -0400

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "note-app-uploadsserverlesstutorial"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "Yhttps://xu8mq2xqg8.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_76tv3LSGK",
+    APP_CLIENT_ID: "63dvpt4pse9998qbnogsdg04hq",
+    IDENTITY_POOL_ID: "us-east-2:1a5fb9c2-ce74-4bb7-afc6-7816879262b1"
+  }
+};
